7817973cda5050a498cfb66f2946828080fbba74
Shudhoo <140329641+Shudhoo@users.noreply.github.com>
2025-06-21 13:16:52 +0530

    merge solve

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 # This is test.py 
 print("Hello World")
+
 print("Hello World for dev branch this is commit fifth/fifth1 from Git-Practice-1")
 print("Hello World for dev branch this is commit fifth from Git-Practice-1 && Git-Practice-2 sisxth commit")
+
+
+print("Hello World for dev branch this is commit fifth from Git-Practice-1")
 
